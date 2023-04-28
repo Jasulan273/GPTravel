4d356ec7111ef3ff9f8de03f80dc7af4b1839a9d
Jasulan273 <54472410+Jasulan273@users.noreply.github.com>
2023-04-29 03:47:55 +0600

    f

---
+++ app/script.js
@@ -0,0 +1,8 @@
+function openForm() {
+    if (document.getElementById("myForm").style.display == "none") {
+        document.getElementById("myForm").style.display = "block";
+    }
+    else if (document.getElementById("myForm").style.display == "block") {
+        document.getElementById("myForm").style.display = "none";
+    }
+}
